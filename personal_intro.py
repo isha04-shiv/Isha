15d556ea95b99a0704fad2a3dcf4a62f4cb08ca8
Isha kumari <itsmeisha04@gmail.com>
2026-02-19 15:30:39 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 12 17:14:03 2026
+
+@author: Rittush
+"""
+
+name=input("what is your name ?")
+old=int(input("How old are you ?"))
+hobby=input("what is your favourite hobby ?")
+university=input("What is  your university name ? ")
+department=input("What is your department name ?")
+stream=input("What is your stream ?")
+print()
+print(f"welcome {name}!")
+print(f"you are {old} years old and you love {hobby}")
+print(f"your university name is {university}")
+print(f"your department and stream are {department} and {stream}")
+print()
